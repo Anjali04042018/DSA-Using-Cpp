23b894ea777e4e17fc84131394c0830f0de8317c
Anjali04040 <anjali04042018@gmail.com>
2024-07-14 22:20:01 +0530

    DSA

--- Recurision/Factorial.cpp
+++ Recurision/Factorial.cpp
@@ -1,21 +1,35 @@
-#include<iostream>
-using namespace std;
-int factorial(int n){
+// #include<iostream>
+// using namespace std;
+// int factorial(int n){
 
-    // Base case
-    if(n==0){
-        return 1;
-    }
-    // Recursive relation
+//     // Base case
+//     if(n==0){
+//         return 1;
+//     }
+//     // Recursive relation
     
-    int smallerProblem= factorial(n-1);
-    int biggerProblem = n * smallerProblem;
-    return biggerProblem;
-}
+//     int smallerProblem= factorial(n-1);
+//     int biggerProblem = n * smallerProblem;
+//     return biggerProblem;
+// }
 
-int main(){
-    int n;
-    cout<< " Enter the value of n : ";
-    cin>> n;
-    cout<< factorial(n);
-}
+// int main(){
+//     int n;
+//     cout<< " Enter the value of n : ";
+//     cin>> n;
+//     cout<< factorial(n);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
