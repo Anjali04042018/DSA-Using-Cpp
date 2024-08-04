ca1b8e1f9f840fcc7ca4bd59cb52ba4726088af5
Anjali04040 <anjali04042018@gmail.com>
2024-08-04 23:55:28 +0530

    Stack revision

--- 14. Stack/revision.cpp
+++ 14. Stack/revision.cpp
@@ -2,33 +2,89 @@
 #include<stack>
 using namespace std;
 
-// int main(){
-//     stack<int> st;
-//     st.push(10);
-//     st.push(12);
-//     st.push(14);
-//     st.push(18);
-//     st.push(20);
-//     cout<< "Top Element is : "<< st.top()<< endl;
-//     st.pop();
-//     cout<< "Top Element is : "<< st.top()<< endl;
+// Stack Implementation using class
 
-//     cout<< st.size()<< endl;
-
-// }
-
-class stack{
+class Stack{
     public:
-    int *arr;
     int size;
+    int *arr;
     int top;
-    stack(int size){
-        this->size = size;
-        arr= new int[size];
+
+    Stack(int size){
+        this ->size= size;
+        arr = new int[size];
         top = -1;
     }
-};
 
+    void push(int data){
+        if(size - top> 1){
+            top++ ;
+            arr[top] = data;
+        }
+        else{
+            cout<< "Stack Overflow"<< endl;
+        }
+    }
+
+    void pop(){
+        if(top >= 0){
+            top--;
+        }
+        else{
+            cout<< "Stck underflow"<< endl;
+        }
+    }
+
+    int peek(){
+        if(top >= 0){
+
+        return arr[top];
+        }else{
+            cout<< "Stack is empty"<< endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty(){
+        if(top == -1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+};
 int main(){
-    stack 
+   // STL of Stack
+
+//    stack<int>st;
+
+//    st.push(10);
+//    st.push(20);
+//    cout<< "top element is : "<< st.top()<< endl;
+//    st.pop();
+//     cout<< "top element is : "<< st.top()<< endl;
+//     if(st.empty()){
+
+//    cout<<"Stack is empty" << endl;
+//     }else{
+//         cout<< "Stack is not empty"<< endl;
+//     }
+
+//     cout<< "Size of stack is " << st.size()<< endl;
+
+    Stack st(5);
+    st.push(10);
+    st.push(20);
+    st.push(30);
+    st.push(40);
+    cout<<"Top element is "<< st.peek()<< endl;
+    st.pop();
+    cout<<"Top Element is "<<  st.peek()<< endl;
+    st.pop();
+    if(st.isEmpty()){
+        cout<< "Stack is Empty"<< endl;
+    }else{
+        cout<< "Stack is not Empty" << endl;
+    }
 }
